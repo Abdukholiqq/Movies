63675c674c9c9a5e81f9474b52455ba5a9d10c6f
Abdukholiqq <sumusddollar@gmail.com>
2022-12-19 16:58:42 +0500

    FullMovie

--- js/main.js
+++ js/main.js
@@ -14,9 +14,9 @@ fetch(`https://www.omdbapi.com/?s=avengers&plot=full&apikey=20295c63`)
         const card = document.createElement("div")
         card.className = "card bg-secondary bg-opacity-50 mb-5"
         card.innerHTML = `
-      <img class="" src="${element.Poster}" alt="img" style="height: 550px;  ">
-      <h3 class= "text-center mt-4 mb-2 text-info">  ${element.Title}</h3>
-            <h4 class="text-center mb-2 text-info" > ${element.Type}  ${element.Year}</h4>
+      <img class="rounded-2" src="${element.Poster}" alt="img" style="height: 550px;  ">
+      <h3 class= "text-center mt-1 mb-2 text-info p-3">  ${element.Title}</h3>
+            <h4 class="text-center mb-2 pb-2 text-info " > ${element.Type} . ${element.Year}</h4>
  `
  card.appendChild(fragment)
  elMain.appendChild(card)
